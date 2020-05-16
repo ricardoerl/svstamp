1e876a0443a06c875f5ff7533c1b9af57740cb42
Ricardo Ramirez <ricardo.ramirezlara@gmail.com>
2020-05-16 00:44:02 -0600

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,53 @@
+import Head from 'next/head';
+import Stamp from '../components/Stamp';
+
+const Home = () => {
+  return (
+    <div>
+      <Head>
+        <meta charset="utf-8" />
+        <title>Inicio</title>==
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#ffffff" />
+        <link
+          href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css"
+          rel="stylesheet"
+        ></link>
+      </Head>
+      <div className="p-5">
+        <div className="text-center sm:py-8">
+          <h1 className="text-3xl">El Salvador Stamp</h1>
+          <p>
+            Colección de tweets de El Salvador marcados utilizando{' '}
+            <a href="https://tweetstamp.org/" className="text-blue-500">
+              @tweet_stamp
+            </a>
+          </p>
+          <form className="max-w-2xl mx-auto mt-6">
+            <div className="flex items-center border-b border-b-2 border-gray-300 py-2">
+              <input
+                className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+                type="text"
+                placeholder="Pegar enlace de @tweet_stamp"
+                aria-label="Enlace de tweet_stamp"
+              />
+              <button
+                className="flex-shrink-0 bg-blue-500 hover:bg-blue-700 border-blue-500 hover:border-blue-700 text-sm border-4 text-white py-1 px-2 rounded"
+                type="button"
+              >
+                Guardar
+              </button>
+            </div>
+          </form>
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 row-gap-2 mt-6">
+          {[...Array(10)].map((item, index) => (
+            <Stamp key={index} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ readme.md
@@ -0,0 +1,12 @@
+# stampi
+
+## Roadmap
+
+- [ ] Show tweets grid from database
+- [ ] Support basic filters (author)
+- [ ] Optimize tailwindcss integration
+- [ ] Add how to contribute
+- [ ] Add pagination support
+- [ ] Add Typescript support
+- [ ] Add testing coverage
+- [ ] Support advance filters (date, text)
