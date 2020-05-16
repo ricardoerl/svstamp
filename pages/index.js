354cc62a435dc05750f7f876aac250d931218c7b
Ricardo Ramirez <ricardo.ramirezlara@gmail.com>
2020-05-16 15:33:56 -0600

    Temporary remove tweets request

--- pages/index.js
+++ pages/index.js
@@ -50,15 +50,15 @@ const Home = ({ tweets = [] }) => {
   );
 };
 
-export async function getStaticProps() {
-  const res = await fetch(`${process.env.API_BASE}/api/tweets`);
-  const tweets = await res.json();
+// export async function getStaticProps() {
+//   const res = await fetch(`${process.env.API_BASE}/api/tweets`);
+//   const tweets = await res.json();
 
-  return {
-    props: {
-      tweets,
-    },
-  };
-}
+//   return {
+//     props: {
+//       tweets,
+//     },
+//   };
+// }
 
 export default Home;
