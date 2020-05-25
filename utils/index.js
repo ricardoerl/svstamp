222ab969b5d3606d742378076309bd172d9d6f58
Tomás Figueroa <teft90@gmail.com>
2020-05-25 11:33:59 -0600

    Actualizar la regex de validación
    Los [snowflakes](https://developer.twitter.com/en/docs/basics/twitter-ids) tienen una longitud máxima de 20 caracteres (UInt64), pero he encontrado tweets del 2009 con una longitud mínima de 10

--- utils/index.js
+++ utils/index.js
@@ -5,7 +5,7 @@ export const getIdFromURL = (url = '') => {
 };
 
 export const validateTweetstampURL = (url = '') => {
-  const regexp = /^https:\/\/tweetstamp\.org\/(\d{19})/;
+  const regexp = /^https:\/\/tweetstamp\.org\/(\d{10,20})/;
   return regexp.test(url);
 };
 
