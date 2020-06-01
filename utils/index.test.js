e0a29b06c67eafadc5a3211adb3d6277a59dbea0
Ricardo Ramirez <ricardo.ramirezlara@gmail.com>
2020-05-31 23:15:19 -0600

    Add initial unit test for date utils

--- utils/index.test.js
+++ utils/index.test.js
@@ -1,4 +1,6 @@
-import { getIdFromURL } from './index';
+import dayjs from 'dayjs';
+import 'dayjs/locale/es';
+import { getIdFromURL, getDateFormat } from './index';
 
 describe('Get ID from a given URL', () => {
   test('should return an id as string', () => {
@@ -8,5 +10,17 @@ describe('Get ID from a given URL', () => {
   test('should return an empty string', () => {
     const badURL = 'tweetstamp.org';
     expect(getIdFromURL(badURL)).toBe('');
+  });
+});
+
+describe('Get local date format', () => {
+  test('should return the current date with correct format', () => {
+    const format = 'MMMM D, YYYY – h:mm A';
+    expect(getDateFormat()).toBe(dayjs(Date()).locale('es').format(format));
+  });
+  test('should return the given date with custom format', () => {
+    const date = 'Tue May 19 08:21:47 +0000 2020';
+    const format = 'MMMM D, YYYY';
+    expect(getDateFormat(date, format)).toBe('Mayo 19, 2020');
   });
 });
